8b824e5de2ee9ff1089ee59bb2cc66b606e99c33
James Dunkerley <james.dunkerley@gmail.com>
2015-10-19 13:54:07 +0100

    Working gh-pages version

--- index.js
+++ index.js
@@ -41,7 +41,7 @@
         setIFrame('<HTML><Body>Loading ...</Body></HTML>');
 
         $.ajax({
-            url: 'examples/' + scriptTarget,
+            url: '  examples/' + scriptTarget,
             success: function(jsCode) {
                 editor.getSession().setValue(jsCode);
                 editor.setReadOnly(false);
